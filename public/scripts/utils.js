4ca55b2ee0fe1c90539c5cf6c2cca4402367deb8
Invertee <smpetch+github@gmail.com>
2018-06-25 17:01:10 +0100

    Added service decoding function

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -42,6 +42,35 @@ function replaceDates(itemclass) {
   }
 }
 
+function toDec(hexstr) {
+  return parseInt("0x" + hexstr, 16);
+}
+
+function decodeServices(flags) {
+  let flag = toDec(flags);
+  let res = "";
+  let services = [
+    ["NONE", 0],
+    ["NETWORK", 1 << 0],
+    ["GETUTXO", 1 << 1],
+    ["BLOOM", 1 << 2],
+    ["WITNESS", 1 << 3],
+    ["XTHIN", 1 << 4],
+    ["NETWORK_LIMITED", 1 << 10]
+  ];
+
+  for (let i = 0; i < services.length; i++) {
+    if ((flag & services[i][1]) != 0) {
+      res += services[i][0] + ", ";
+    }
+  }
+  return res.slice(0, -2);
+}
+
+function formatHex(hex) {
+  return "0x" + hex.replace(/^0+/, "");
+}
+
 function onLoad() {
   replaceBytes("bytes");
   replaceDates("connectiontime");
